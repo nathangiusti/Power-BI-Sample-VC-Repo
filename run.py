fffd3fe27c9bb83cc473f4319adaa9d7bbe8cfcb
Nathan Giusti <bigbadbyte2@gmail.com>
2021-08-31 14:20:30 -0400

    test

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+import json
 import sys
-print("This is the name of the script:", sys.argv[0])
-print("Number of arguments:", len(sys.argv))
-print("The arguments are:" , str(sys.argv))
+
+
+for i in range(1, len(sys.argv)):
+    if sys.argv[i].endswith('.json'):
+        with open(sys.argv[i], 'r') as f:
+            json_str = json.dumps(json.load(f), indent=4)
+        with open(sys.argv[i], 'w') as f:
+            f.write(json_str)
+        print('Pretty Printed {}'.format(sys.argv[i]))
