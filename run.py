4b640eb2715375addf3561696740c72af6f831bb
Nathan Giusti <bigbadbyte2@gmail.com>
2021-08-31 14:35:02 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import zipfile
 import json
 import sys
 
@@ -9,3 +10,17 @@ for i in range(1, len(sys.argv)):
         with open(sys.argv[i], 'w') as f:
             f.write(json_str)
         print('Pretty Printed {}'.format(sys.argv[i]))
+    if sys.argv[i].endswith('.pbix'):
+        zf = zipfile.ZipFile(sys.argv[i])
+        data = json.loads(zf.read('Report/Layout').decode('utf-16-le'))
+        data['config'] = json.loads(data['config'])
+        for section in data['sections']:
+            for visual_container in section['visualContainers']:
+                for key in ['config', 'filters', 'query', 'dataTransforms']:
+                    if key in visual_container.keys():
+                        visual_container[key] = json.loads(visual_container[key])
+        output_path = sys.argv[i][:-5] + '.json'
+        print(output_path)
+        print(data)
+        with open(output_path, "w") as f:
+            json.dump(data, f, indent=4)
