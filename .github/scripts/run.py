24309e555604da40a8a7c08c8a0e400317d51810
Nathan Giusti <bigbadbyte2@gmail.com>
2021-10-11 13:52:28 -0400

    Moved to folder, testing paths

--- .github/scripts/run.py
+++ .github/scripts/run.py
@@ -5,4 +5,9 @@ import sys
 def main():
     print('Number of arguments:', len(sys.argv), 'arguments.')
     print('Argument List:', str(sys.argv))
+
+    for file in sys.argv:
+        if file.endswith(".pbix"):
+            print(file)
+            
     print("Done")
