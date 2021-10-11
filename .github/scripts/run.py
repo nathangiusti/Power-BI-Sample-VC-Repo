b97edb6838334524d02fa574b9eca28b84d63701
Nathan Giusti <bigbadbyte2@gmail.com>
2021-10-11 14:13:04 -0400

    added secrets

--- .github/scripts/run.py
+++ .github/scripts/run.py
@@ -1,13 +1,22 @@
+import os
+
 import requests
-import sys
+
+tenant_id = 'cef04b19-7776-4a94-b89b-375c77a8f936'
 
 
 def main():
-    print('Number of arguments:', len(sys.argv), 'arguments.')
-    print('Argument List:', str(sys.argv))
 
-    for file in sys.argv:
-        if file.endswith(".pbix"):
-            print(file)
-            
-    print("Done")
+    data = {
+        'client_id': os.environ['CLIENT_ID'],
+        'grant_type': 'client_credentials',
+        'resource': 'https://analysis.windows.net/powerbi/api',
+        'response_mode': 'query',
+        'client_secret': os.environ['SECRET']
+    }
+    resp = requests.get('https://login.microsoftonline.com/{}/oauth2/token'.format(tenant_id), data=data)
+
+    access_token = resp.json()['access_token']
+
+    print(access_token)
+    
